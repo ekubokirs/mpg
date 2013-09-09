c8e8187b30844edda7c11cd3ad76cd0f9ca8df07
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2013-09-09 15:39:25 -0700

    added some styling

--- spec/features/user_adds_new_tank_spec.rb
+++ spec/features/user_adds_new_tank_spec.rb
@@ -4,9 +4,9 @@ feature "user creates tank", js: true do
 	scenario "creating a tank" do
 		visit '/tanks/new'
 
-		fill_in 'Amount', with: '12.5'
-		fill_in 'Price', with: '$15.70'
-		fill_in 'Current Milage', with: '150,000'
+		fill_in 'Amount in Gallons', with: '12.5'
+		fill_in 'Price per Gallon', with: '$15.70'
+		fill_in 'Miles on This Tank', with: '150,000'
 
 
 		click_button 'Submit'
